1984abb0cb2f388cd5f2d538acec7a43825a9e5a
Francisco Franco <franciscofranco.1990@gmail.com>
2018-03-23 01:58:55 +0000

    Fix my derp when merging https://github.com/sultanxda/android_kernel_oneplus_msm8998/commit/71fed1fd322b2d8396465b1513f06bf22203bbf8
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1285,7 +1285,6 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 	int ret = 0;
 	cpumask_t allowed_mask;
 
-	new_mask = adjust_cpumask(p, new_mask);
 	rq = task_rq_lock(p, &flags);
 
 	/*

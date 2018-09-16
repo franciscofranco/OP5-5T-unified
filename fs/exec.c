092cb62ec93bdd50756dc23a2752e47b96973b6d
Francisco Franco <franciscofranco.1990@gmail.com>
2018-09-16 01:24:56 +0000

    fs: exec: mistakenly removed Lineage's custom is 'is_su' code block
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1653,6 +1653,11 @@ static int do_execveat_common(int fd, struct filename *filename,
 	if (retval < 0)
 		goto out;
 
+	if (is_su && capable(CAP_SYS_ADMIN)) {
+		current->flags |= PF_SU;
+		su_exec();
+	}
+
 	if (capable(CAP_SYS_ADMIN)) {
 		if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN)))
 			atomic_set(&zygote32_pid, current->pid);
